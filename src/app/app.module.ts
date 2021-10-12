93da7fe93cf345702e3058e0e44a0ceb5af221fb
mattriney <61891660+mattriney@users.noreply.github.com>
2021-10-12 00:11:57 -0500

    remove movie item, reorg with movie details comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MovieLibraryComponent } from './movie-library/movie-library.component';
 import { MovieDetailsComponent } from './movie-library/movie/movie-details/movie-details.component';
-import { MovieItemComponent } from './movie-library/movie/movie-details/movie-item/movie-item.component';
+import { TitleComponent } from './movie-library/movie/movie-details/title/title.component';
+import { CoverArtComponent } from './movie-library/movie/movie-details/cover-art/cover-art.component';
+import { StarsComponent } from './movie-library/movie/movie-details/stars/stars.component';
+import { SynopsisComponent } from './movie-library/movie/movie-details/synopsis/synopsis.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { MovieItemComponent } from './movie-library/movie/movie-details/movie-it
     HeaderComponent,
     MovieLibraryComponent,
     MovieDetailsComponent,
-    MovieItemComponent
+    TitleComponent,
+    CoverArtComponent,
+    StarsComponent,
+    SynopsisComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movie-library/movie/movie-details/cover-art/cover-art.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cover-art',
+  templateUrl: './cover-art.component.html',
+  styleUrls: ['./cover-art.component.css']
+})
+export class CoverArtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
