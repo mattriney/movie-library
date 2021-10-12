48c315c7be5268d999afaca2fcb8ec2a2a60bcfc
mattriney <61891660+mattriney@users.noreply.github.com>
2021-10-11 23:54:59 -0500

    update/create component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MoviesComponent } from './movies/movies.component';
-import { MovieComponent } from './movie/movie.component';
+import { MovieLibraryComponent } from './movie-library/movie-library.component';
+import { MovieDetailsComponent } from './movie-library/movie/movie-details/movie-details.component';
+import { MovieItemComponent } from './movie-library/movie/movie-details/movie-item/movie-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MoviesComponent,
-    MovieComponent
+    MovieLibraryComponent,
+    MovieDetailsComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule,
