92be55b3bb8e590f64da32fe6b9914433a082604
mattriney <61891660+mattriney@users.noreply.github.com>
2021-10-14 19:49:06 -0500

    remove routing stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,4 +7,29 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+// const appRoutes: Routes = [
+//   { path: 'crisis-center', component: CrisisListComponent },
+//   { path: 'hero/:id',      component: HeroDetailComponent },
+//   {
+//     path: 'heroes',
+//     component: HeroListComponent,
+//     data: { title: 'Heroes List' }
+//   },
+//   { path: '',
+//     redirectTo: '/heroes',
+//     pathMatch: 'full'
+//   },
+//   { path: '**', component: PageNotFoundComponent }
+// ];
+
+// @NgModule({
+//   imports: [
+//     RouterModule.forRoot(
+//       appRoutes,
+//       { enableTracing: true } // <-- debugging purposes only
+//     )
+//     // other imports here
+//   ],
+//   ...
+// })
 export class AppRoutingModule { }
