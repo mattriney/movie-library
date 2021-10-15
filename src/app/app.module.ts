982ab1919ff6e9e09291c5af35b2d330610c8d4f
mattriney <61891660+mattriney@users.noreply.github.com>
2021-10-14 19:49:22 -0500

    remove routing stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,29 +37,4 @@ import { MovieEditComponent } from './movie-library/movie/movie-edit/movie-edit.
   providers: [],
   bootstrap: [AppComponent]
 })
-// const appRoutes: Routes = [
-//   { path: 'crisis-center', component: CrisisListComponent },
-//   { path: 'hero/:id',      component: HeroDetailComponent },
-//   {
-//     path: 'heroes',
-//     component: HeroListComponent,
-//     data: { title: 'Heroes List' }
-//   },
-//   { path: '',
-//     redirectTo: '/heroes',
-//     pathMatch: 'full'
-//   },
-//   { path: '**', component: PageNotFoundComponent }
-// ];
-
-// @NgModule({
-//   imports: [
-//     RouterModule.forRoot(
-//       appRoutes,
-//       { enableTracing: true } // <-- debugging purposes only
-//     )
-//     // other imports here
-//   ],
-//   ...
-// })
 export class AppModule { }
